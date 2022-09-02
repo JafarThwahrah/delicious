371623a84be78a80c8607e29dd6a70905ab54144
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-09-02 21:00:24 +0300

    g

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import {Splide , SplideSlide} from '@splidejs/react-splide'
-import '@splidejs/splide/dist/css/splide.min.css';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 function Popular() {
   const [popular, setPopular] = useState([]);
 
@@ -10,33 +10,44 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch("https://digimon-api.herokuapp.com/api/digimon");
-    const data = await api.json();
-    let array = [];
-    for (let i = 0; i < 10; i++) {
-      array.push(data[i]);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch("https://digimon-api.herokuapp.com/api/digimon");
+      const data = await api.json();
+      let array = [];
+      for (let i = 0; i < 10; i++) {
+        array.push(data[i]);
+      }
+      localStorage.setItem("popular", JSON.stringify(array));
+      setPopular(array);
     }
-    setPopular(array);
   };
 
   return (
     <div>
       <Wrapper>
         <h3> Popular Picks </h3>
-        <Splide options = {{
-          perPage:4,
-        }}>
-
-        {popular.map((recipe) => {
-          return (
-            <SplideSlide>
-            <Card>
-              <p>{recipe.name}</p>
-              <img src={recipe.img} alt={recipe.name} />
-            </Card>
-            </SplideSlide>
-          );
-        })}
+        <Splide
+          options={{
+            perPage: 4,
+            pagination: false,
+            drag: "free",
+            gap: "2rem",
+          }}>
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.name}</p>
+                  <img src={recipe.img} alt={recipe.name} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
         </Splide>
       </Wrapper>
     </div>
@@ -45,20 +56,50 @@ function Popular() {
 
 const Wrapper = styled.div`
   margin: 4rem 0rem;
+  padding: 1rem;
+  background-color: grey;
 `;
 
 const Card = styled.div`
-min-height= 25rem;
 border-radius:2rem
 overflow: hidden;
-background-color:red;
+height: 25rem;
 padding:1rem;
-margin:1rem;
-width:fit-content;
+border-radius:2rem;
 img{
-  border-radius:50%;
+  border-radius:2rem;
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
+p{
+  position: absolute;
+  z-index:10;
+  left:50%;
+  bottom: 0%;
+  transform: translate(-50% , 0%);
+  color:blue;
+  width:100%;
+  text-align: center;
+  font-size:1rem;
+  font-weight:600;
+  height:40%;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba (0, 0, 0, 0.5));
 `;
 
 export default Popular;
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Veggie() {
+  const [veggie, setVeggie] = useState([]);
+
+  useEffect(() => {
+    getVeggie();
+  }, []);
+
+  const getVeggie = async () => {
+    const check = localStorage.getItem("veggie");
+
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apikey=${process.env.REACT_APP_API_KEY}&number=9&tags=vegetarian`);
+      const data = await api.json();
+     
+
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+      setVeggie(data.recipes);
+
+    }
+  };
+
   return (
-    <div>Veggie</div>
-  )
+    <div>
+      <Wrapper>
+        <h3> Our vegiterian picks </h3>
+        <Splide
+          options={{
+            perPage: 3,
+            pagination: false,
+            drag: "free",
+            gap: "2rem",
+          }}>
+          {veggie.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.name}</p>
+                  <img src={recipe.img} alt={recipe.name} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 }
 
-export default Veggie
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+  padding: 1rem;
+  background-color: grey;
+`;
+
+const Card = styled.div`
+border-radius:2rem
+overflow: hidden;
+height: 25rem;
+padding:1rem;
+border-radius:2rem;
+img{
+  border-radius:2rem;
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+p{
+  position: absolute;
+  z-index:10;
+  left:50%;
+  bottom: 0%;
+  transform: translate(-50% , 0%);
+  color:blue;
+  width:100%;
+  text-align: center;
+  font-size:1rem;
+  font-weight:600;
+  height:40%;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba (0, 0, 0, 0.5));
+`;
+
+export default Veggie;
