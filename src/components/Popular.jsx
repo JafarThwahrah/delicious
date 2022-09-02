95b01e5357b6b44d0c86662bc911fd661a141fbb
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-09-02 17:24:39 +0300

    m

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,59 +1,64 @@
-import { useEffect , useState } from 'react';
-import styled from 'styled-components';
-
-
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import {Splide , SplideSlide} from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css';
 function Popular() {
-const [popular , setPopular]= useState([]);
-
+  const [popular, setPopular] = useState([]);
 
   useEffect(() => {
-    getPopular()
-  }, []); 
+    getPopular();
+  }, []);
 
+  const getPopular = async () => {
+    const api = await fetch("https://digimon-api.herokuapp.com/api/digimon");
+    const data = await api.json();
+    let array = [];
+    for (let i = 0; i < 10; i++) {
+      array.push(data[i]);
+    }
+    setPopular(array);
+  };
 
-const getPopular = async () => {
-const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-  const data = await api.json();
-  setPopular(data.recipes);
-};
+  return (
+    <div>
+      <Wrapper>
+        <h3> Popular Picks </h3>
+        <Splide options = {{
+          perPage:4,
+        }}>
 
-
-
-  return  <div>
-    {popular.map((recipe) => {
-      return(
-
-        <Wrapper>
-          <h3>Popular Picks</h3>
-          {popular.map((recipe) => {
-            return(
-                <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                </Card>
-            )
-          })}
-        </Wrapper>
-        
-      );
-    })}
-  </div>;
-  
+        {popular.map((recipe) => {
+          return (
+            <SplideSlide>
+            <Card>
+              <p>{recipe.name}</p>
+              <img src={recipe.img} alt={recipe.name} />
+            </Card>
+            </SplideSlide>
+          );
+        })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 }
 
 const Wrapper = styled.div`
-margin: 4rem 0rem;
+  margin: 4rem 0rem;
 `;
 
 const Card = styled.div`
 min-height= 25rem;
-<border-radius:2rem>
+border-radius:2rem
 overflow: hidden;
+background-color:red;
+padding:1rem;
+margin:1rem;
+width:fit-content;
+img{
+  border-radius:50%;
+}
 
-  img{
-    border-radius:2rem;
-  }
 `;
 
-
-export default Popular
+export default Popular;
