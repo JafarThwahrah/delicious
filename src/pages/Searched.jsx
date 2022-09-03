8a122260495e6c209c545a2f142b3c19a0e84f60
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-09-03 18:40:11 +0300

    l

---
+++ src/pages/Searched.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useEffect , useState} from "react";
+import {useParams} from 'react-router-dom';
+import styled from 'styled-components';
+
+function Searched() {
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
+    let params = useParams();
+    const getSearched = async (name) =>{
+        const data = await fetch(`https://digimon-api.herokuapp.com/api/digimon/complexSearch&query=${name}`);
+        const recipes = await data.json();
+        setSearchedRecipes(recipes.results);
+      };
+
+      useEffect(() => {
+       getSearched(params.search);
+      }, [params.search]);
+
+
+
+  return (
+    <Grid>
+        {searchedRecipes.map((item) =>{
+          return (
+            <Card key={item.id}>
+                <img src={item.img} alt="" />
+                <h4>{item.name}</h4>
+                </Card>
+          )
+        })}
+    </Grid>
+  )
+}
+
+
+
+
+const Grid = styled.div`
+display:grid;
+grid-template-columns: repeat(auto-fit, minmax(1fr));
+grid-gap:3rem;
+`
+const Card = styled.div`
+ img {
+    width: 100%;
+    border-radius:2rem;
+ }
+
+ a {
+    text-decoration:none;
+ }
+ h4 {
+    text-align:center;
+    padding: 1rem;
+ }
+`
+
+
+
+
+
+
+
+export default Searched
